05fc79610397dae82058795c0ffcb754c8af2a3c
AnaBeatriz030 <159550896+AnaBeatriz030@users.noreply.github.com>
2025-04-04 23:23:24 -0300

    inst admin/ final

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('biao trem do odio!') }}
                 </div>
             </div>
         </div>
